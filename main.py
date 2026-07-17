901d31002034acd43819b443e3308a87e3847232
Mukund Jha <mukundzha33@gmail.com>
2026-07-17 14:38:39 +0545

    add feed selection menu

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import requests
+import webbrowser
+
 from rich.console import Console
 
-console = Console()
+input_feed = input("Choose Feed : \n1. Top Stories\n2. New Stories\n3. Best Stories\nEnter your choice (1-3): ")
+if input_feed == "1":
+    feed_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+elif input_feed == "2":
+    feed_url = "https://hacker-news.firebaseio.com/v0/newstories.json"
+elif input_feed == "3":
+    feed_url = "https://hacker-news.firebaseio.com/v0/beststories.json"
+else:
+    print("Invalid choice. Defaulting to Top Stories.")
+    feed_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 
+console = Console()
 limit = int(input("Enter the number of top stories to display (max 30): "))
 
 if not 1 <= limit <= 30:
@@ -11,30 +23,35 @@ if not 1 <= limit <= 30:
 
 try:
     story_ids = requests.get(
-        "https://hacker-news.firebaseio.com/v0/beststories.json",
+        f"{feed_url}",
         timeout=10,
     ).json()
 except requests.RequestException:
     console.print("[bold red]Failed to fetch Hacker News stories.")
-    raise SystemExit
+    exit()
+
+stories = []
 
 console.print(f"\n[bold cyan]Top {limit} Hacker News Stories\n")
 
 for number, story_id in enumerate(story_ids[:limit], start=1):
     try:
         story = requests.get(
-            f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json",
             timeout=10,
+            url=f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json",
         ).json()
     except requests.RequestException:
         continue
 
-    title = story.get("title", "No title")
-    author = story.get("by", "Unknown")
-    score = story.get("score", 0)
-    url = story.get("url", "No URL")
+    stories.append(story)
 
-    console.print(f"[bold cyan]{number}.[/] {title}")
-    console.print(f"[dim]{author} • ⭐ {score}")
-    console.print(f"[blue]{url}")
+    console.print(f"[bold cyan]{number}. {story.get('title', 'No title')}")
+    console.print(f"[dim]{story.get('by', 'Unknown')} • ⭐ {story.get('score', 0)}")
+    console.print(f"[blue]{story.get('url', 'No URL')}")
     console.print()
+
+choice = int(input("Enter story number to open (0 to skip): "))
+
+if choice != 0:
+    webbrowser.open(stories[choice - 1]["url"])
+
